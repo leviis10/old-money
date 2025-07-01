da39c88077f77c0545f4b36f66cc1a6c7c7437c7
Levi Indrajaya Sutantio <alevindrajaya@gmail.com>
2025-07-01 22:30:21 +0700

    Add JWT Utils

---
+++ app/src/utils/jwt_utils.rs
@@ -0,0 +1,54 @@
+use crate::entities::roles::Model as RolesModel;
+use crate::entities::users::Model as UsersModel;
+use jsonwebtoken::{EncodingKey, Header};
+use serde::{Deserialize, Serialize};
+use time::{Duration, OffsetDateTime};
+
+#[derive(Serialize, Deserialize)]
+enum Roles {
+    ADMIN,
+    UNKNOWN,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Claims {
+    sub: String,
+    roles: Vec<Roles>,
+    iat: usize,
+    exp: usize,
+}
+
+impl Claims {
+    pub fn new(user_model: &UsersModel, roles_model: &Vec<RolesModel>) -> Claims {
+        let now = OffsetDateTime::now_utc();
+        let iat = now.unix_timestamp();
+        let exp = (now + Duration::days(30)).unix_timestamp();
+        let sub = user_model.username.clone();
+        let roles: Vec<Roles> = roles_model
+            .iter()
+            .map(|role_model| {
+                return if role_model.name == "ADMIN" {
+                    Roles::ADMIN
+                } else {
+                    Roles::UNKNOWN
+                };
+            })
+            .collect();
+        Claims {
+            sub,
+            roles,
+            iat: iat as usize,
+            exp: exp as usize,
+        }
+    }
+}
+
+pub fn generate_token(claims: Claims) -> String {
+    let secret = "secret";
+    jsonwebtoken::encode(
+        &Header::default(),
+        &claims,
+        &EncodingKey::from_secret(secret.as_ref()),
+    )
+    .unwrap()
+}
---
+++ migration/src/m20250701_134445_create_m2m_user_roles_tables.rs
@@ -0,0 +1,125 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // create `Users` table
+        manager
+            .create_table(
+                Table::create()
+                    .table(Users::Table)
+                    .if_not_exists()
+                    .col(pk_auto(Users::Id))
+                    .col(string(Users::Username))
+                    .col(string(Users::Email))
+                    .col(string(Users::Password))
+                    .col(timestamp_with_time_zone(Users::CreatedAt).default(Expr::current_timestamp()))
+                    .col(timestamp_with_time_zone(Users::UpdatedAt).default(Expr::current_timestamp()))
+                    .col(timestamp_with_time_zone_null(Users::DeletedAt))
+                    .to_owned(),
+            )
+            .await?;
+
+        // create `Roles` table
+        manager
+            .create_table(
+                Table::create()
+                    .table(Roles::Table)
+                    .if_not_exists()
+                    .col(pk_auto(Roles::Id))
+                    .col(string(Roles::Name))
+                    .col(timestamp_with_time_zone(Roles::CreatedAt).default(Expr::current_timestamp()))
+                    .col(timestamp_with_time_zone(Roles::UpdatedAt).default(Expr::current_timestamp()))
+                    .col(timestamp_with_time_zone_null(Roles::DeletedAt))
+                    .to_owned(),
+            )
+            .await?;
+
+        // create `UserRoles` table
+        manager
+            .create_table(
+                Table::create()
+                    .table(UserRoles::Table)
+                    .if_not_exists()
+                    .col(integer(UserRoles::UserId))
+                    .col(integer(UserRoles::RoleId))
+                    .primary_key(
+                        Index::create()
+                            .name("pk-user_roles")
+                            .col(UserRoles::UserId)
+                            .col(UserRoles::RoleId)
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("fk-user_roles-user_id")
+                            .from(UserRoles::Table, UserRoles::UserId)
+                            .to(Users::Table, Users::Id)
+                            .on_delete(ForeignKeyAction::Cascade)
+                            .on_update(ForeignKeyAction::Cascade)
+                    )
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("fk-user_roles-role_id")
+                            .from(UserRoles::Table, UserRoles::RoleId)
+                            .to(Roles::Table, Roles::Id)
+                            .on_delete(ForeignKeyAction::Cascade)
+                            .on_update(ForeignKeyAction::Cascade)
+                    )
+                    .to_owned(),
+            )
+            .await?;
+
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        // drop `UserRoles` table
+        manager
+            .drop_table(Table::drop().table(UserRoles::Table).to_owned())
+            .await?;
+        
+        // drop `Roles` table
+        manager
+            .drop_table(Table::drop().table(Roles::Table).to_owned())
+            .await?;
+        
+        // drop `Users` table
+        manager
+            .drop_table(Table::drop().table(Users::Table).to_owned())
+            .await?;
+        
+        Ok(())
+    }
+}
+
+#[derive(DeriveIden)]
+enum Users {
+    Table,
+    Id,
+    Username,
+    Email,
+    Password,
+    CreatedAt,
+    UpdatedAt,
+    DeletedAt
+}
+
+#[derive(DeriveIden)]
+enum Roles {
+    Table,
+    Id,
+    Name,
+    CreatedAt,
+    UpdatedAt,
+    DeletedAt
+}
+
+#[derive(DeriveIden)]
+enum UserRoles {
+    Table,
+    UserId,
+    RoleId
+}
